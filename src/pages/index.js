4e8d3b08493336c0a879f33f94cdb6fda3bab20d
youxiue <44764161+youxiue@users.noreply.github.com>
2021-12-16 09:54:06 +0800

    部署测试
    更改内容

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,8 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title">我的网站</h1>
+        <p className="hero__subtitle">我的网站超级酷</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
